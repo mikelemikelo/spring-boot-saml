86c40f614d327a895e5819631795ce38e68eeff0
Miguel Rosas <mikelemikelo@gmail.com>
2023-08-04 14:36:40 -0600

    Spring SAML2.0 Okta integration version 1.0 for Java 17

---
+++ src/test/java/com/modelop/springbootsaml/SpringBootSamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelop.springbootsaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
